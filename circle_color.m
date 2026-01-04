f68f8c182079e508e2cb4e0db87044bf431219f7
Athen-ol <anto@lamaisonenpaille.com>
2026-01-04 20:35:26 +0100

    uistack removed

--- circle_color.m
+++ circle_color.m
@@ -3,10 +3,10 @@ function circle_color(x,y,r,color)
 %r is the radius of the circle
 %0.01 is the angle step, bigger values will draw the circle faster but
 %you might notice imperfections (not very smooth)
-ang=0:0.01:2*pi; 
+ang=0:0.01:2*pi;
 xp=r*cos(ang);
 yp=r*sin(ang);
 %%c = plot(x+xp,y+yp,'Color',color);
 h = fill(x+xp,y+yp, [0.4 0.4 0.4], 'EdgeColor', color, 'LineWidth', 3);
-uistack(h, 'top'); 
+%uistack(h, 'top');
 end
